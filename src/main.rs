0f5ce89ee4ef3f1fe7ea02e16e37695d4893830f
Tobias Hunger <tobias.hunger@slint-ui.com>
2023-07-14 12:20:18 +0200

    SHOW: Functional UI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ui_project_rs::add;
+use ui_project_rs::{add, div, mul, sub};
 
 use slint::slint;
 
@@ -48,11 +48,57 @@ export component AppWindow inherits Window {
 
 }
 
+enum Operation {
+    None,
+    Add(i32),
+    Sub(i32),
+    Mul(i32),
+    Div(i32),
+}
+
 fn main() -> Result<(), slint::PlatformError> {
     let app = AppWindow::new()?;
 
-    app.on_key_pressed(|k: slint::SharedString| {
-        eprintln!("Key pressed: {k}");
+    let a = app.as_weak();
+    let mut op = Operation::None;
+
+    app.on_key_pressed(move |k: slint::SharedString| {
+        let a = a.upgrade().unwrap();
+        match k.as_bytes()[0] {
+            c if c >= b'0' && c <= b'9' => {
+                let current = a.get_current_value();
+                let result = current * 10 + ((c - b'0') as i32);
+                a.set_current_value(result);
+            },
+            b'A' => {
+                op = Operation::Add(a.get_current_value());
+                a.set_current_value(0);
+            }
+            b'S' => {
+                op = Operation::Sub(a.get_current_value());
+                a.set_current_value(0);
+            }
+            b'M' => {
+                op = Operation::Mul(a.get_current_value());
+                a.set_current_value(0);
+            }
+            b'D' => {
+                op = Operation::Div(a.get_current_value());
+                a.set_current_value(0);
+            }
+            b'=' => {
+                let o2 = a.get_current_value();
+                match op {
+                    Operation::None => {},
+                    Operation::Add(o1) => a.set_current_value(add(o1, o2)),
+                    Operation::Sub(o1) => a.set_current_value(sub(o1, o2)),
+                    Operation::Mul(o1) => a.set_current_value(mul(o1, o2)),
+                    Operation::Div(o1) => a.set_current_value(div(o1, o2)),
+                }
+                op = Operation::None;
+            }
+            _ => todo!(),
+        };
     });
 
     app.run()?;
