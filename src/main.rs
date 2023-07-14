21e72d9dcac12f110e04bd91a368da6b84998ef4
Tobias Hunger <tobias.hunger@slint-ui.com>
2023-07-14 11:38:14 +0200

    Done: Hello World UI

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use ui_project_rs::add;
 
-fn main() {
+use slint::slint;
+
+slint! {
+    export component AppWindow inherits Window {
+        Text { text: "Hello World!"; }
+    }
+}
+
+fn main() -> Result<(), slint::PlatformError> {
     let result = add(4, 3);
     println!("Hello, world! 4 + 3 = {result}");
+
+    let app = AppWindow::new()?;
+
+    app.run()?;
+
+    Ok(())
 }
